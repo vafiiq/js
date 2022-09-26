0670beb661c94bd3b99c3c871d2b01d44b3a833e
Mirziyod-saidov <saidov.mirziyod@mail.ru>
2022-09-26 22:43:13 +0500

    js

---
+++ index.js
@@ -0,0 +1,45 @@
+"use strict"
+
+// 1 - ex
+// let a = +prompt("Enter the length:");
+// console.log(`${a} sm =${a / 100} metr `);
+// document.write(`${a} sm =${a / 100} metr `);
+
+// 2 - ex
+// let b = +prompt("enter the weight");
+// console.log(`${b} sm = ${b / 1000} kg `);
+// document.write(`${b} sm = ${b / 1000} kg `);
+
+// 3 - ex
+// let A = +prompt("enter A"),
+//     B = +prompt("enter B");
+// if (A > B) {
+//     console.log(` A to the  B  ${A / B} times `);
+// }
+// else {
+//     console.log("you write something wrong! Try again.");
+// }
+
+
+// 4 - ex
+// let c = +prompt(" enter two-digit number");
+// console.log(`tens' room digit ${Math.trunc(c / 10)} ones room digit ${c % 10}`);
+
+
+// 5 - ex
+// let d = +prompt(" enter two-digit number");
+// console.log(`${Math.trunc(d / 10) + (d % 10)}`);
+
+
+// 6 - ex
+// let e = +prompt(" enter two-digit number");
+// console.log(`${Math.trunc(e / 10) * (e % 10)}`);
+
+// 7 - ex
+// let f = +prompt("enter a number greater than 999");
+// console.log(`${(f % 1000) / 100}`);
+
+
+// 8 - ex
+// let g = +prompt(" enter how many seconds past");
+// console.log(` since the start of the day ${g / 60} minutes , ${g / 3600} and hour past`);
